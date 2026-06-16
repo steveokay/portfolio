89387af7147a46a1c2cfe1c9c2f794e9d7ec0b2d
Steve Mokay <mokaysteve@gmail.com>
2026-06-17 00:20:43 +0300

    feat: static sitemap.xml with rel=sitemap link in head

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
